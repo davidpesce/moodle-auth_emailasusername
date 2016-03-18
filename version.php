2818d7ac5c8fa9f4eb3e148972c86437d575d67a
David Pesce <davidpesce@gmail.com>
2016-03-18 16:16:33 -0400

    changed version number to give a bit of leeway for changes

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016031800;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2016030000;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015111000;        // Requires this Moodle version
 $plugin->component = 'auth_emailusername';      // Full name of the plugin (used for diagnostics)

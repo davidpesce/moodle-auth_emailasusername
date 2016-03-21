cc7cf195c11aaf7f4981ffcb28a7d0b5898c877a
David Pesce <davidpesce@gmail.com>
2016-03-21 14:55:45 -0400

    Fix - Plugin name not showing correctly.

--- auth.php
+++ auth.php
@@ -37,7 +37,6 @@ class auth_plugin_emailasusername extends auth_plugin_base {
     public function __construct() {
         $this->authtype = 'emailasusername';
         $this->config = get_config('auth/emailasusername');
-        $this->title = get_string('pluginname', 'auth_emailasusername');
     }
 
     /**

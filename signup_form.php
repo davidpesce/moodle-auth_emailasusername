168f89f9eff8798214960cb7c9ebde8bd774208d
David Pesce <davidpesce@gmail.com>
2017-01-25 17:02:36 -0500

    updated plugin due to change in rendering in Moodle 3.2

--- signup_form.php
+++ signup_form.php
@@ -13,10 +13,11 @@ require_once($CFG->libdir.'/formslib.php');
 require_once($CFG->dirroot.'/user/profile/lib.php');
 require_once($CFG->dirroot . '/user/editlib.php');
 
-class login_signup_form_emailasusername extends moodleform {
+class login_signup_form extends moodleform implements renderable, templatable{
     function definition() {
         global $USER, $CFG;
         $mform = $this->_form;
+
         $mform->addElement('header', 'createuserandpass', get_string('auth_emailasusername_signupheader', 'auth_emailasusername'), '');
 
         $mform->addElement('text', 'username', get_string('auth_emailasusername_email', 'auth_emailasusername'), 'maxlength="100" size="35"');
@@ -47,6 +48,7 @@ class login_signup_form_emailasusername extends moodleform {
         }
 
         profile_signup_fields($mform);
+
         if ($this->signup_captcha_enabled()) {
             $mform->addElement('recaptcha', 'recaptcha_element', get_string('security_question', 'auth'), array('https' => $CFG->loginhttps));
             $mform->addHelpButton('recaptcha_element', 'recaptcha', 'auth');
@@ -62,14 +64,17 @@ class login_signup_form_emailasusername extends moodleform {
         // buttons
         $this->add_action_buttons(true, get_string('createaccount'));
     }
+
     function definition_after_data(){
         $mform = $this->_form;
         $mform->applyFilter('username', 'trim');
+
         // Trim required name fields.
         foreach (useredit_get_required_name_fields() as $field) {
             $mform->applyFilter($field, 'trim');
         }
     }
+
     function validation($data, $files) {
         global $CFG, $DB;
         $errors = parent::validation($data, $files);
@@ -129,6 +134,7 @@ class login_signup_form_emailasusername extends moodleform {
         $errors += profile_validation($dataobject, $files);
         return $errors;
     }
+
     /**
      * Returns whether or not the captcha element is enabled, and the admin settings fulfil its requirements.
      * @return bool
@@ -138,4 +144,22 @@ class login_signup_form_emailasusername extends moodleform {
         $authplugin = get_auth_plugin($CFG->registerauth);
         return !empty($CFG->recaptchapublickey) && !empty($CFG->recaptchaprivatekey) && $authplugin->is_captcha_enabled();
     }
+
+    /**
+     * Export this data so it can be used as the context for a mustache template.
+     *
+     * @param renderer_base $output Used to do a final render of any components that need to be rendered for export.
+     * @return array
+     */
+    public function export_for_template(renderer_base $output) {
+        ob_start();
+        $this->display();
+        $formhtml = ob_get_contents();
+        ob_end_clean();
+        $context = [
+            'formhtml' => $formhtml
+        ];
+        return $context;
+    }
+
 }
